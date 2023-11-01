79f5c4feeb69d0c8e3ce119d0edda4a233aa1243
Lucas Guerega <124807748+LucasGuerega@users.noreply.github.com>
2023-11-01 09:39:11 -0300

    Update Calcular.java

--- RadioButton/src/modelo/Calcular.java
+++ RadioButton/src/modelo/Calcular.java
@@ -1,5 +1,7 @@
 package modelo;
 
+import java.text.DecimalFormat;
+
 public class Calcular {
 	public int add(int num1, int num2) {
 		return num1 + num2;
@@ -13,8 +15,14 @@ public class Calcular {
 		return num1 * num2;
 	}
 
-	public int divi(int num1, int num2) {
-		return num1 / num2;
+	public String divi(int num1, int num2) {
+		float num1F = Float.valueOf(num1);
+		float num2F = Float.valueOf(num2);
+		DecimalFormat decimalFormat = new DecimalFormat("0.00");
+		float divi = num1F / num2F;
+		String txtresultado = decimalFormat.format(divi);
+
+		return txtresultado;
 	}
 
 }
