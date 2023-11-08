3b79b66872d9629065a4f4de94604f0aa51856e1
Lucas Guerega <lucas.g2006@aluno.ifsc.edu.br>
2023-11-08 10:22:42 -0300

    Sistema de Posto de Combustível
    sendo feito

--- Sistema de Posto de Combustivel/ifsc/modelo/Calculos.java
+++ Sistema de Posto de Combustivel/ifsc/modelo/Calculos.java
@@ -1,5 +1,22 @@
 package modelo;
 
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+
 public class Calculos {
+	public Float calcAbast(float digiQntL, String String[] ) {
+		
+		if (String[]= 1)
+			{
+					JOptionPane.showMessageDialog(null, "Hello Wolrd");
+			}
+		
+		return digiQntL;
+	}
+
+	/*public float calcAbast(float digiQntL, JComboBox<String> comboBoxString) {
+		// TODO Auto-generated method stub
+		return 0;
+	}*/
 
 }
